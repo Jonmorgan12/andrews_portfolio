4f4e76c558fa438b441af19a8f64faa4ab1ecc27
Jon-Morgan <jonmorgan.cobb@gmail.com>
2021-10-04 10:11:01 -0500

    css changed in contact form

--- my-app/src/Components/Contact/Contact.js
+++ my-app/src/Components/Contact/Contact.js
@@ -5,15 +5,38 @@ export default function Contact() {
   return (
     <div className="form-container">
       <h1 className="form-title">CONTACT FORM</h1>
+      <h2 className="form-title">
+        Send me an email, I look forward to do buisness with you.
+      </h2>
       <form>
-        <label>Name</label>
-        <input className="input-box" type="text" name="fullName" />
-        <label>Email Adress</label>
-        <input className="input-box" type="text" name="email" />
-        <label>Subject</label>
-        <input className="input-box" type="text" name="subject" />
-        <label>Message</label>
-        <textarea className="large-text-box" type="text" name="message" />
+        {/* <label>Name</label> */}
+        <input
+          className="input-box"
+          type="text"
+          name="fullName"
+          placeholder="Name"
+        />
+        {/* <label>Email Adress</label> */}
+        <input
+          className="input-box"
+          type="text"
+          name="email"
+          placeholder="Email Adress"
+        />
+        {/* <label>Subject</label> */}
+        <input
+          className="input-box"
+          type="text"
+          name="subject"
+          placeholder="Subject"
+        />
+        {/* <label>Message</label> */}
+        <textarea
+          className="large-text-box"
+          type="text"
+          name="message"
+          placeholder="Message to Andrew..."
+        />
       </form>
       <div style={{ overflow: "auto" }}>
         <button className="send-message">Send Message</button>
